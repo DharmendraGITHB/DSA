ce9e62863a12d7b4aa3c55f7dd025fc54a9a466e
Dharmendra Kumar <57281417+DharmendraGITHB@users.noreply.github.com>
2022-03-07 23:56:44 +0530

    node

--- linkedlist/node.py
+++ linkedlist/node.py
@@ -1,1 +1,17 @@
+#singly linked list
 
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+# linked list class
+
+class linkedlist:
+
+    def __init__(self):
+        self.head = None
+
+
+
+  
