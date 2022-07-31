9e7379f6d1538e7f5e4d7b92db3a835c3a351d40
Dharmendra Kumar <57281417+DharmendraGITHB@users.noreply.github.com>
2022-07-31 21:36:54 +0530

    insert a node at beginning

---
+++ linkedlist/insert.py
@@ -0,0 +1,41 @@
+# program for inserting a node at the front of the linked list
+#node class 
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Linkedlist:
+    def __init__(self):
+        self.head= None
+    #inserting new node at the beginning
+    def push(self, data):
+        new_node= Node(data)
+
+        new_node.next= self.head
+
+        self.head = new_node
+
+    #printing linkedlist 
+    def printl(self):
+        start= self.head
+        while start:
+            print(start.data, end= " ")
+            start = start.next
+            
+if __name__ == '__main__':
+
+    llist = Linkedlist()
+
+    llist.push(7)
+    llist.push(8)
+    llist.push(9)
+
+    llist.printl()
+
+
+
+# time complexity : O(n)
+    
+
+        
