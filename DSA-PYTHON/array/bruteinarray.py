67dd3595e876134eab6225ca70738a0cf0370b62
Dharmendra Kumar <57281417+DharmendraGITHB@users.noreply.github.com>
2023-01-31 01:01:06 +0530

    brute force in array

---
+++ DSA-PYTHON/array/bruteinarray.py
@@ -0,0 +1,38 @@
+class bruteforce:
+	def __init__(self, arr):
+		self.array = array
+
+
+	def search(self, target):
+		for i in range(len(self.array)):
+			if self.array[i] == target:
+				return i
+
+
+		return -1
+
+
+arr = [1,2,3,4,5,6,7,8,9]
+target = 5
+check = bruteforce(array)
+
+if result != -1:
+	print("target found at index:", result)
+
+else:
+	print("target not found in the array")
+
+
+""" time complexity of this code is O(n), where n is the number of elements in the array.
+this is worst case. the algo wil have to iterate through all n elements in the array 
+
+
+
+space complexity o(1)
+
+
+to improve time complexity of this we need to try other algorithms like binary search.
+
+dk
+
+"""
