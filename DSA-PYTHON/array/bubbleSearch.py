db7d034214024df5a77798149d3ba82e841eefca
Dharmendra Kumar <57281417+DharmendraGITHB@users.noreply.github.com>
2023-09-25 13:40:26 +0530

    bubble search

---
+++ DSA-PYTHON/array/bubbleSearch.py
@@ -0,0 +1,24 @@
+#bubble sort algorithm
+
+def bubble_sort(arr):
+	for i in range(len(arr)-1):  
+		for j in range(len(arr)-i-1): 
+			if arr[j] > arr[j+1]:
+				arr[j],arr[j+1] = arr[j+1], arr[j]
+
+	return arr
+
+def main():
+
+	
+	user_input = int(input("enter the number of input YOU WANT: "))
+
+	#arr = []
+	for i in range(user_input):
+		element = input(f"enter a element {i+1}: ")
+		arr.append(element)
+
+	print('sorted array', bubble_sort(arr))
+
+if  __name__ == "__main__":
+	main()
