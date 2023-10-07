4dba420092964f7fd5ffbc594fb45ba50703e94b
Dharmendra Kumar <57281417+DharmendraGITHB@users.noreply.github.com>
2023-10-07 09:45:39 +0530

    string reverse using recursion

--- DSA-PYTHON/Recursion/string.py
+++ DSA-PYTHON/Recursion/string.py
@@ -19,3 +19,47 @@ print(fun)
 # space complexity of this recursive algorithm is O(n) as well, This is because for each recursive call a new stack frame is created to store the functions local variables 
 
 """ this is not the optimal solution """
+
+""" 
+reverse_string_recursive("Hello, World!")
+  reverse_string_recursive("ello, World!")
+    reverse_string_recursive("llo, World!")
+      reverse_string_recursive("lo, World!")
+        reverse_string_recursive("o, World!")
+          reverse_string_recursive(", World!")
+            reverse_string_recursive(" World!")
+              reverse_string_recursive("World!")
+                reverse_string_recursive("orld!")
+                  reverse_string_recursive("rld!")
+                    reverse_string_recursive("ld!")
+                      reverse_string_recursive("d!")
+                        reverse_string_recursive("!") (Base case)
+
+
+
+reverse("hello")
+|
+|--- reverse("ello") + "h"
+|    |
+|    |--- reverse("llo") + "e"
+|    |    |
+|    |    |--- reverse("lo") + "l"
+|    |    |    |
+|    |    |    |--- reverse("o") + "l"
+|    |    |    |    |
+|    |    |    |    |--- reverse("") + "o"
+|    |    |    |    |    |
+|    |    |    |    |    |--- ""
+|    |    |    |    |  
+|    |    |    |    |--- "o"
+|    |    |    |
+|    |    |    |--- "ol"
+|    |    |    |  
+|    |    |--- "llo"
+|    |  
+|    |--- "olle"
+|  
+|--- "olleh"
+
+
+"""
